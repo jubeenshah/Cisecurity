71f8f79be24fd1fec8a4f8047d50680e7da71c98
Jubeen Shah <jubeen.shah@lexisnexis.com>
2022-05-02 08:54:23 -0400

    update with controls

--- cis1804.sh
+++ cis1804.sh
@@ -2244,9 +2244,7 @@ lev && (
     update_conf /etc/sysctl.d/local.conf 'net.ipv4.tcp_congestion_control=bbr'    #BBR
 )
 
-qte || (
-    echo -e "\n\n\n\n######################################"
-    echo -e "\nSystem is hardened." 
-    echo -e "\n######################################"
-)
-
+# qte || (
+# echo -e "\n\n\n\n######################################"
+# echo -e "\nSystem is hardened." 
+# echo -e "\n######################################")
